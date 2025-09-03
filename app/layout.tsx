52045f7fe45fd8aec79d6ec7f450f319a4a8a7cd
Abraham <abrahamnhier97@gmail.com>
2025-09-03 19:18:08 +0300

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Jost } from "next/font/google";
+import "./globals.css";
+
+const jost = Jost({
+  variable: "--font-jost",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Solemate",
+  description: "An e-commerce platform for Nike shoes",
+  icons: {
+    icon: "/images/logo.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${jost.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
